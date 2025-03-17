d3f2827ead94e4c1203a62e9de9ff424d3b4d9de
ndrscodes <98schmidtandreas@gmail.com>
2025-03-18 00:27:39 +0100

    feat: implement levenshteinDistance for tag search

--- taginator.js
+++ taginator.js
@@ -19,6 +19,25 @@ async function getTags(search){
     return await new scraper().getJson('https://rapidtags.io/api/generator?query=' + search + 'type=YouTube')
 }
 
+const levenshteinDistance = (s, t) => {
+	if (!s.length) return t.length;
+	if (!t.length) return s.length;
+	const arr = [];
+	for (let i = 0; i <= t.length; i++) {
+		arr[i] = [i];
+		for (let j = 1; j <= s.length; j++) {
+			arr[i][j] =
+				i === 0
+				? j
+					: Math.min(
+						arr[i - 1][j] + 1,
+						arr[i][j - 1] + 1,
+						arr[i - 1][j - 1] + (s[j - 1] === t[i - 1] ? 0 : 1));
+		}
+	}
+	return arr[t.length][s.length];
+};
+
 async function getProcessedTags(search){
     let tags = await getTags(search);
     tags = tags.tags;
@@ -35,7 +54,7 @@ async function getProcessedTags(search){
         }
         else{
             searchItems.forEach(str => {
-                if(!added && element.toString().toLowerCase().includes(str.toLowerCase())){
+                if(!added && levenshteinDistance(str.toLowerCase(), element.toLowerCase()) < 8){
                     hitCount++;
                     console.log(element + " is a probable candidate: " + hitCount + " matches with title")
                     if(hitCount >= searchItems.length/3){
